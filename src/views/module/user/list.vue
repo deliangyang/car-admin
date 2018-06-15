cfb1f1f9eb6b806f554624cc43c2725787c3bc57
ydl <yangdeliang@juwang.cn>
2018-06-15 17:45:03 +0800

    some vue

---
+++ src/views/module/user/list.vue
@@ -0,0 +1,34 @@
+<template>
+
+</template>
+<script>
+    export default {
+        name: 'list',
+        data() {
+            return {
+                user: []
+            }
+        },
+        created() {
+
+        },
+        methods: {
+            listRequest: function(page) {
+                this.$axios.get('/admin/user/list', {params: {page: page}}).then(function(res) {
+                    console.log(res)
+                    this.user.list = res.data
+                    this.total = res.data.total
+                    this.current_page = res.data.current_page
+                    this.article = res.data.data
+                    this.page_size = res.data.per_page
+                })
+            }
+        },
+        destroyed() {
+
+        },
+    }
+</script>
+
+<style>
+</style>
